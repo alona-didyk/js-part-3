779b2eaefdb9048790250b8f319da753c864e84c
Alona Didyk <alionadiduk31@gmail.com>
2023-11-08 23:17:40 +0200

    new

--- starter/script.js
+++ starter/script.js
@@ -1,38 +1,38 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// high-level - we dont have to manage resources by ourselves cus they have abstractions that take all the work from us
 
-// DOM document object model - structured representation of HTML documents. Allows js to access HTML elements and styles to manipulate them
-// Document is an entry point to them
-// HTML element is a root element
+// garbage collection - engine inside js which automatically removes old unused objects from comp memory
 
-// DOM IS NOT PART OF THE JS, ITS PART OF WEB API`S(APLICATION PROGRAMMING INTERFACE) - ITS A LIBRARYS (ALSO WRITEN IN JS) THAT BROWSER IMPLEMENT AND WE CAN ACCESS THEM USING JS
+// interpreted - comp processer only undrstand zero and ones(machine code)
 
-// to get text content
-// mostly used with div, span, p
-// contains the plain text content of the element, including all child nodes
-// document.querySelector('.message').textContent = 'Correct number!';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
+// multi-paradigm - js does all three
+// paradigm is an approach and an overall mindset of structuring our code, which will ultimately direct the coding style and technique in a project that uses certain paradigm
+// three popular paradigm: - procedural programming(organising the code in a very linear way and with some functions in between); - object-oriented programming; - functional programming
 
-// // to get value
-// // mostly used with input, textarea,select
-// // reprsents the value entered or selected by the user in a form element
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
+// prototype-based object-oriented
 
-// event is something that happens on the page
-// with event listener we can can wait for something to happen and then react to this
+// first-class functions - functions are treated as variables
 
-const number = Math.trunc(Math.random() * 20) + 1;
+// dynamic - dynamically typed: no data-type definition; data-type of variables automatically changed
 
-// first specify the event and then tell what to do through the function(event handler)
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  //   document.querySelector('.message').textContent = 'Correct number!';
-  console.log(guess, typeof guess);
+// single-threaded - js can do one thing at a time
+// conccurency model - how js engine handles multiple tasks happening at the same time
 
-  if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
-  }
-});
+// non-bloking event loop - takes long-running tasks, execute them in the 'background' and puts them back in the main thread once they are finished
+
+// js engine - programm that executes js code
+// engine contains: -callstack(where our code executes); - heap(where objects are stored)
+
+// compilation - the entire code is converted into machine code at once, and written to a binary file that can be executed by a computer
+
+// enterpretation - interpreter runs through the source code and executes it line by line
+
+// just-in-time compilation - entire code is converted into machine code at once, then executed immediately
+// Steps:
+// parsing - read the code, the code is parsed into abstract syntax tree - used to generate machine code(not related to DOM)
+// compilation - takes generated AST and compiles it to machine code
+// execution
+// optimisation - the code is optimised and recompiled during the already running programm execution
+
+// runtime in the browser - container including all the things that we need to use js. Engine, WEB API, callback queue(all the callback functions that are ready to be executed) are required
