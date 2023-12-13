f36476e6fa785403834d8315df74a141460b28a4
Alona Didyk <alionadiduk31@gmail.com>
2023-12-13 12:32:51 +0200

    starter

--- starter/script.js
+++ starter/script.js
@@ -1,38 +1,29 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// DOM document object model - structured representation of HTML documents. Allows js to access HTML elements and styles to manipulate them
-// Document is an entry point to them
-// HTML element is a root element
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-// DOM IS NOT PART OF THE JS, ITS PART OF WEB API`S(APLICATION PROGRAMMING INTERFACE) - ITS A LIBRARYS (ALSO WRITEN IN JS) THAT BROWSER IMPLEMENT AND WE CAN ACCESS THEM USING JS
-
-// to get text content
-// mostly used with div, span, p
-// contains the plain text content of the element, including all child nodes
-// document.querySelector('.message').textContent = 'Correct number!';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
-
-// // to get value
-// // mostly used with input, textarea,select
-// // reprsents the value entered or selected by the user in a form element
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
-// event is something that happens on the page
-// with event listener we can can wait for something to happen and then react to this
-
-const number = Math.trunc(Math.random() * 20) + 1;
-
-// first specify the event and then tell what to do through the function(event handler)
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  //   document.querySelector('.message').textContent = 'Correct number!';
-  console.log(guess, typeof guess);
-
-  if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
-  }
-});
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
