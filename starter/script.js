377aeb73ef702c4172452ccf463743fd254e2308
Alona Didyk <alionadiduk31@gmail.com>
2023-12-16 20:47:31 +0200

    lection 2

--- starter/script.js
+++ starter/script.js
@@ -528,3 +528,119 @@ const restaurant = {
 
 // USE OBJECTS IF YOU NEED MORE 'TRADITIONAL' KEY/VALUE STORE, EASIER TO WRITE AND ACCESS VALUES WITH . AND []; USE WHEN YOU NEED TO INCLUDE FUNCTIONS(METHODS), USE WHEN WORKING WITH JSON
 // USE MAPS IF YOU NEED BETTER PERFORMANCE(SIMPLE KEY/VALUES STORES), KEYS CAN HAVE ANY DATA TYPE, EASY TO ITERATE, EASY TO COMPUTE(CALCULATE); USE WHEN YOU SIMPLY NEED TO MAP KEY TO VALUES; USE WHEN YOU NEED KEYS THAT ARE NOT STRINGS
+
+// STRINGS
+
+const airline = 'TAP Air Port';
+// const plane = 'A320';
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log(plane[3]);
+
+// console.log('B737'[0]);
+
+// to read length
+// console.log(airline.length);
+// console.log('B737'.length);
+
+// methods
+// the first
+// console.log(airline.indexOf('r'));
+// the last
+// console.log(airline.lastIndexOf('r'));
+// to search for a word
+// console.log(airline.indexOf('Port'));
+
+// slice method
+// The slice() method extracts a part of a string
+// The slice() method returns the extracted part in a new string
+// The slice() method does not change the original string
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ')));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+
+// const checkMiddleSeat = function (seat) {
+// B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') {
+//     console.log('You got the middle seat');
+//   } else {
+//     console.log('You got lucky');
+//   }
+// };
+
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
+// Changing the case of a string
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'jOnAs';
+const passengerToLowerCase = passenger.toLowerCase();
+const passengerCorrrect =
+  passengerToLowerCase[0].toUpperCase() + passengerToLowerCase.slice(1);
+console.log(passengerCorrrect);
+
+// Check email
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+const lowerEmail = loginEmail.toLowerCase();
+// to delete white spaces
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+
+// to replace all the above
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+
+console.log(email === normalizedEmail);
+
+// To replace parts of string
+const priceGB = '288,97&';
+const priceUS = priceGB.replace('&', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
+
+// regular expression
+// /g - global
+console.log(announcement.replace(/door/g, 'gate'));
+
+// methods that returns booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of a NEW Airbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are not allowed on board');
+  } else {
+    console.log('Welcome on board!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
